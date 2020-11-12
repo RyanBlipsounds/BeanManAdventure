e916cf301a24f787a5916cd4059a5fe7243b19fe
RyanBlipsounds <ryanstunkel@blipsounds.com>
2020-11-11 20:27:24 -0600

    Stuff

---
+++ Bean Man Gets His Cool Back Unity Proj/Assets/Scripts/ActManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActManager : MonoBehaviour
+{
+    public bool fadeToBlack;
+    public SpriteRenderer BlackScreen;
+    public Color ColorValue;
+
+    public GameState m_gameState;
+
+    public float timeToFade = 0;
+
+
+    void Start()
+    {
+        BlackScreen = GameObject.Find("BlackScreen").GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LoadNewAct();
+
+        if (m_gameState.beanState == GameState.gameState.IsCool)
+        {
+            Debug.Log("BEEAN");
+        }
+        else if (m_gameState.beanState == GameState.gameState.IsNotCool) {
+            Debug.Log("MEN");
+        }
+        else if (m_gameState.beanState == GameState.gameState.IsBagged)
+        {
+            Debug.Log("UNITE!!");
+        }
+    }
+
+    //In Load New Act, it should probably take dictionary arguments 
+    public void LoadNewAct()
+    {
+        float tmp;
+
+        timeToFade += Time.deltaTime;
+
+        tmp = Mathf.Lerp(1, 0, timeToFade / 2);
+
+        Debug.Log(tmp);
+
+        BlackScreen.color = new Color(0, 0, 0, tmp);
+
+        if (tmp == 0) {
+
+        }
+    }
+
+}
---
+++ Bean Man Gets His Cool Back Unity Proj/Assets/Scripts/UIController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public GameObject StartPoint;
+    public GameObject EndPoint;
+    public float speed = 1.0f;
+
+    public GameObject ResponseBox;
+    public GameObject DialogueBox;
+    public GameObject CanTalkBox;
+
+    public bool isActive = false;
+    public bool hasMoved = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ResponseBox = GameObject.Find("ResponseBox");
+        //DialogueBox = GameObject.Find("DialogueBox");
+        //CanTalkBox = GameObject.Find("CanTalkBox");
+
+        //CanTalkBox.GetComponent<UIController>().isActive;
+
+        transform.position = StartPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isActive)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, EndPoint.transform.position, Time.deltaTime * speed);
+        }
+        else {
+            transform.position = Vector2.MoveTowards(transform.position, StartPoint.transform.position, Time.deltaTime * speed);
+        }
+    }
+}
