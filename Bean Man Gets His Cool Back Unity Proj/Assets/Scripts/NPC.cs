46026b33ac1c137e94feffebcc2b0311b36798be
RyanBlipsounds <ryanstunkel@blipsounds.com>
2020-11-17 22:53:50 -0600

    fixed untracked files

--- Bean Man Gets His Cool Back Unity Proj/Assets/Scripts/NPC.cs
+++ Bean Man Gets His Cool Back Unity Proj/Assets/Scripts/NPC.cs
@@ -6,11 +6,19 @@ public class NPC : MonoBehaviour
 {
     public bool hasSpoken = false;
     public GameObject glasses = default;
+    public GameObject noGlasses = default;
+
     public GameState gameState = default;
+
+    public void Start()
+    {
+        glasses.SetActive(false);
+        noGlasses.SetActive(true);
+    }
 
     public void ShowGlasses()
     {
         glasses.SetActive(true);
+        noGlasses.SetActive(false);
     }
-
 }
