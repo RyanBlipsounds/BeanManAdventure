1c079a283be198279a74f14da6bc580309090ee2
Anthony Davis <anthony.davis33@gmail.com>
2020-11-21 11:33:13 -0500

    new menu started

--- Bean Man Gets His Cool Back Unity Proj/Assets/Scripts/UILogic.cs
+++ Bean Man Gets His Cool Back Unity Proj/Assets/Scripts/UILogic.cs
@@ -3,5 +3,28 @@ using UnityEngine.UI;
 
 public class UILogic : MonoBehaviour
 {
+    // Main Menu Game Objects
+    public GameObject MainMenu;
+    public Button MMPlayButton;
+
+
+
+    private void Start()
+    {
+        MMPlayButton.onClick.AddListener(OnButtonAClicked);
+    }
+    private void OnDestroy()
+    {
+        MMPlayButton.onClick.RemoveListener(OnButtonAClicked);
+    }
+    private void OnButtonAClicked()
+    {
+        MainMenu.SetActive(false);
+        // Allow Movement
+    }
+    private void OnButtonBClicked()
+    {
+        
+    }
 
 }
